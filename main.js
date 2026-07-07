5a27fb5f62e2f7cbbe56f7e32c82b3c67769c6a5
Lary4V <hillary.vitoriano@escola.pr.gov.br>
2026-07-07 07:58:30 -0300

    07/07/2026

---
+++ main.js
@@ -0,0 +1,11 @@
+const botoes = document.querySelector All (".botao");
+
+for Celet i = 0 ;i<botoes.length;i++){
+botoes [|]. onclick = function{
+for Celet j = 0; j<botoes.length;J++0
+
+}
+
+botoes[j]. Classlist.add("ativo")
+};
+}
